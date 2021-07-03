2fc5a831dbde5bbd93c6fe2f3c0bc26ce12b5f2a
Shashank Kumar <abc@shanky.xyz>
2021-07-03 17:24:55 +0530

    Enhancement

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+document.querySelector("#curtain").classList.add("animate");
+
+var elementsToSlideFromLeft = document.querySelectorAll(".slide-from-left");
+elementsToSlideFromLeft.forEach(element => {
+    element.style.display = "float";
+    element.style.transform = "translateX(-200%)";
+});
+var elementsToSlideFromRight = document.querySelectorAll(".slide-from-right");
+elementsToSlideFromRight.forEach(element => {
+    element.style.display = "float";
+    element.style.transform = "translateX(+200%)";
+});
+
+function isElementInViewportHeight(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+}
+
+function addAnimationClass() {
+    elementsToSlideFromLeft.forEach(element => {
+        if (isElementInViewportHeight(element)) {
+            element.classList.add("slide-to-original-position");
+        }
+    });
+    elementsToSlideFromRight.forEach(element => {
+        if (isElementInViewportHeight(element)) {
+            element.classList.add("slide-to-original-position");
+        }
+    });
+}
+
+window.addEventListener("load", () => {
+    window.setTimeout(() => {
+        addAnimationClass()
+        window.addEventListener('scroll', function() {
+            addAnimationClass();
+        });
+    }, 1500);
+});
+
+
 document.querySelector(".mobile-nav-button").addEventListener("click", () => {
     document.querySelector("nav.container").style.display = "flex";
     document.querySelector(".mobile-close-nav-button").style.display = "block";
@@ -7,3 +51,13 @@ document.querySelector(".mobile-close-nav-button").addEventListener("click", () 
     document.querySelector("nav.container").style.display = "none";
     document.querySelector(".mobile-close-nav-button").style.display = "none";
 });
+
+document.querySelectorAll("nav.container a").forEach(
+    (selector) => selector.addEventListener(
+        "click", () => {
+            if ((window.innerWidth || document.documentElement.clientWidth) <= 800) {
+                document.querySelector("nav.container").style.display = "none";
+                document.querySelector(".mobile-close-nav-button").style.display = "none";
+            }
+    })
+);
